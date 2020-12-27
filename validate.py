9abcc196f7fb6c35ae0d96d541f6ec54a5dfc811
Severin Schols <severin@schols.de>
2020-12-28 00:48:16 +0100

    check tilesets as well

--- validate.py
+++ validate.py
@@ -1,5 +1,6 @@
 import requests
 import argparse
+import json
 
 # Globals
 
@@ -8,13 +9,16 @@ map_ok = set()
 map_to_parse = set()
 map_parsed = set()
 
+url_failed = set()
+url_ok = set()
+
 # Functions
 
 def url_clean(url):
     #return url.replace("//","/").replace("https:/","https://").split("#")[0]
     return url.split("#")[0].strip()
 
-def check_url(url):
+def check_map_url(url):
     if url in map_failed:
         return False
     if url in map_ok:
@@ -34,6 +38,25 @@ def check_url(url):
         map_failed.add(url)
         return False
 
+def check_generic_url(url):
+    if url in url_failed:
+        return False
+    if url in url_ok:
+        return True
+
+    try:
+        r = requests.head(url)
+    except requests.exceptions.ConnectionError:
+        url_failed.add(url)
+        return False
+
+    if r.ok:
+        url_ok.add(url)
+        return True
+    else:
+        url_failed.add(url)
+        return False
+
 def parse_map(url, print_success = False):
     if url in map_parsed:
         try:
@@ -48,7 +71,19 @@ def parse_map(url, print_success = False):
     has_error = False
 
     r = requests.get(url)
-    data = r.json()
+    try:
+        data = r.json()
+    except json.decoder.JSONDecodeError:
+        if not print_success:
+            print(url)
+        print("  ! invalid JSON")
+        map_parsed.add(url)
+        try:
+            map_to_parse.remove(url)
+        except KeyError:
+            pass
+        return
+
 
     for layer in data["layers"]:
         if "properties" in layer:
@@ -56,7 +91,7 @@ def parse_map(url, print_success = False):
                 if prop["name"] == "exitSceneUrl" or prop["name"] == "exitUrl":
                     next = requests.compat.urljoin(url, prop["value"])
                     next = url_clean(next)
-                    is_ok = check_url(next)
+                    is_ok = check_map_url(next)
 
                     if is_ok:
                         if print_success:
@@ -66,6 +101,20 @@ def parse_map(url, print_success = False):
                             print(url)
                         print("  - %s" % next)
                         has_error = True
+
+    for tileset in data["tilesets"]:
+        ts_url = requests.compat.urljoin(url, tileset["image"])
+        ts_url = url_clean(ts_url)
+        is_ok = check_generic_url(ts_url)
+
+        if is_ok:
+            if print_success:
+                print("  + %s" % ts_url)
+        else:
+            if not print_success and not has_error:
+                print(url)
+            print("  - %s" % ts_url)
+            has_error = True
 
     map_parsed.add(url)
     try:
