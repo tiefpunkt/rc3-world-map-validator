25bf3bf4fede907ff9fbe7fea435608431542f33
Severin Schols <severin@schols.de>
2020-12-28 16:34:16 +0100

    add schedule, artifact, and error handling to tileset check

--- validate.py
+++ validate.py
@@ -101,20 +101,22 @@ def parse_map(url, print_success = False):
                             print(url)
                         print("  - %s" % next)
                         has_error = True
+    try:
+        for tileset in data["tilesets"]:
+            ts_url = requests.compat.urljoin(url, tileset["image"])
+            ts_url = url_clean(ts_url)
+            is_ok = check_generic_url(ts_url)
 
-    for tileset in data["tilesets"]:
-        ts_url = requests.compat.urljoin(url, tileset["image"])
-        ts_url = url_clean(ts_url)
-        is_ok = check_generic_url(ts_url)
-
-        if is_ok:
-            if print_success:
-                print("  + %s" % ts_url)
-        else:
-            if not print_success and not has_error:
-                print(url)
-            print("  - %s" % ts_url)
-            has_error = True
+            if is_ok:
+                if print_success:
+                    print("  + %s" % ts_url)
+            else:
+                if not print_success and not has_error:
+                    print(url)
+                print("  - %s" % ts_url)
+                has_error = True
+    except:
+        pass
 
     map_parsed.add(url)
     try:
